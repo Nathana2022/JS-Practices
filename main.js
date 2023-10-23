aa1e8eef9c794cfbff6965d702ec8c112741e4dd
DonK <nathanaeldon495@gmail.com>
2023-10-23 18:51:08 +0100

    Declare a variable

---
+++ main.js
@@ -0,0 +1,7 @@
+//Wich value does x have after execution of the following code?
+let x ='Geeta'
+//value of x is Geeta
+
+//Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
+let flower='rose';
+let tree = 'maple'
