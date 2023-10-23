3fccf30ab8c3c080a4f94ba7c555a0c4db702187
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2023-10-23 18:26:59 +0100

    Création de la fonction echo

---
+++ index.js
@@ -0,0 +1,45 @@
+//Wich value does x have after execution of the following code?
+let x ='Geeta'
+//value of x is Geeta
+
+//Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
+let flower='rose';
+let tree = 'maple'
+
+//Which value does x have after execution of the following code?
+let y='Tic';
+y = 'Tac';
+y = 'Toe';
+console.log(y)
+//Value does x after execution is Toe
+
+let a='Laurel';
+let b='Hardy';
+let c=b;
+b=a;
+a=c // Value of x is Hardy
+
+function a(){
+  return 'Hello.';
+}
+let Salut = a();
+
+function b(){
+  return 'Hello.'
+}
+let Salut1 = b();
+
+function greet(){
+  return 'Haydo.';
+}
+let Salutation=greet();
+
+function hello(){
+  return 'Hi.'
+}
+let k=hello();
+//The value of x is 'Hi.'
+
+function echo(sound){
+  return sound;
+}
