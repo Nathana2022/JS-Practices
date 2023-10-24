2f994760795c5543c70732960d683aa41d2ea047
DonK <nathanaeldon495@gmail.com>
2023-10-24 18:38:42 +0100

    Function countVowel in string

--- index.js
+++ index.js
@@ -162,3 +162,20 @@ function onesDigit(n){
 function mean(a,b){
   return (a+b)/2;
 }
+
+//Write function that returns number of vowels in string
+
+// program to count the number of vowels in a string
+
+function countVowel(str) { 
+
+  // find the count of vowels
+  const count = str.match(/[aeiou]/gi).length;
+
+  // return number of vowels
+  return count;
+}
+// take input
+const string = prompt('Enter a string: ');
+const result = countVowel(string);
+console.log(result);
